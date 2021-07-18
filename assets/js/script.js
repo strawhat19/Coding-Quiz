9cb319aba76c55377a3331c821d5d0d9a667aed7
Rakib Ahmed <rakib987@gmail.com>
2021-07-18 19:31:24 -0400

    Questions display on button click

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,57 +13,59 @@ var totalQuestions = $('.totalQuestions');
 var cardList = main.children().children().eq(4);
 var gameButton = $('.gameButton');
 
-var quizBox = $('<div class="contain question quizBox"><div class="questionTitle quizBoxTitle"><h1 class="spacer questionTitleText"></h1><div class=iconContainer id=itemContainer><div class="customIcon transition userPoints"title=Points>0</div><div class="customIcon transition iconTimer"title=Timer>60</div><div class="customIcon transition help"title=Help>?</div></div></div><div class=lineSep></div><h2 class="outOf questionIndex"></h2><p class="spacer questionP">Please choose one of the following answers!<ul class="answerChoices list-group"><li class="answer list-item">Variables<li class="answer list-item">Event Listeners<li class="answer list-item">DOM Maniplutaion<li class="answer list-item">Functions</ul></div>');
-
 // Quiz Variables
 var questions = [
-    question1 = {
+    {
         question: 'Hi How Are You?',
+        index: 'Question 1 out of 10',
         choices: ['Good','OK','Fantastic','Bad'],
         answer: 'Good'
     },
-    question2 = {
+    {
         question: 'Hi How Are You Today?',
+        index: 'Question 2 out of 10',
         choices: ['Nope','EH','Hanging On','Dying'],
         answer: 'Dying'
     },
-    question3 = {
-        question: 'Hi How Are You Today?',
+    {
+        question: 'Hi How Are You Yesterday?',
+        index: 'Question 3 out of 10',
         choices: ['Nope','EH','Hanging On','Dying'],
         answer: 'Dying'
     },
-    question4 = {
+    {
         question: 'Hi How Are You Today?',
+        index: 'Question 4 out of 10',
         choices: ['Nope','EH','Hanging On','Dying'],
         answer: 'Dying'
     },
-    question5 = {
+    {
         question: 'Hi How Are You Today?',
+        index: 'Question 4 out of 10',
         choices: ['Nope','EH','Hanging On','Dying'],
         answer: 'Dying'
     },
-    question6 = {
+    {
         question: 'Hi How Are You Today?',
+        index: 'Question 5 out of 10',
         choices: ['Nope','EH','Hanging On','Dying'],
         answer: 'Dying'
     },
-    question7 = {
+    {
         question: 'Hi How Are You Today?',
+        index: 'Question 6 out of 10',
         choices: ['Nope','EH','Hanging On','Dying'],
         answer: 'Dying'
     },
-    question2 = {
+    {
         question: 'Hi How Are You Today?',
+        index: 'Question 7 out of 10',
         choices: ['Nope','EH','Hanging On','Dying'],
         answer: 'Dying'
     },
-    question8 = {
+    {
         question: 'Hi How Are You Today?',
-        choices: ['Nope','EH','Hanging On','Dying'],
-        answer: 'Dying'
-    },
-    question9 = {
-        question: 'Hi How Are You Today?',
+        index: 'Question 8 out of 10',
         choices: ['Nope','EH','Hanging On','Dying'],
         answer: 'Dying'
     }
@@ -76,16 +78,26 @@ function beginGame() {
     console.log(cardParagraph);
     console.log(cardList);
     console.log(gameButton);
-    $(event.target).parent().hide();
-    console.log(gameButton);
 }
 
     main.on('click', '.gameButton', function(event) {
+
         beginGame();
-        for (var i = 0; i < questions.length; i++) {
-            $(event.target).parent().parent().append(quizBox);
-        }
-        // $(event.target).parent().children()
+        $(event.target).parent().hide();
+
+            questions.forEach(element => {
+                var quizBox = $('<div class="contain question questionBox quizBox"><div class="questionTitle quizBoxTitle"><h1 class="spacer questionTitleText"></h1><div class=iconContainer id=itemContainer><div class="customIcon transition userPoints"title=Points>0</div><div class="customIcon transition iconTimer"title=Timer>60</div><div class="customIcon transition help"title=Help>?</div></div></div><div class=lineSep></div><h2 class="outOf questionIndex"></h2><p class="spacer questionP">Please choose one of the following answers!<ul class="answerChoices list-group"><li class="answer list-item">Variables<li class="answer list-item">Event Listeners<li class="answer list-item">DOM Maniplutaion<li class="answer list-item">Functions</ul></div>');
+
+                for (var i = 0; i < questions.length; i++) {
+                    var questionTitle = $('.questionTitleText');
+                    var questionIndex = $('.questionIndex');
+                    questionTitle.text(questions[i].question);
+                    questionIndex.text(questions[i].index);
+                }
+
+                $(event.target).parent().parent().append(quizBox);
+
+            });
     })
 
 // Function Invokations
