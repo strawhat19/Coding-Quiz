cfedaf9fd858ed7daa2917eeb95112df867f3a43
Rakib Ahmed <rakib987@gmail.com>
2021-07-18 19:58:57 -0400

    Question Content Populated

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,56 +16,62 @@ var gameButton = $('.gameButton');
 // Quiz Variables
 var questions = [
     {
-        question: 'Hi How Are You?',
+        question: 'Question One?',
         index: 'Question 1 out of 10',
         choices: ['Good','OK','Fantastic','Bad'],
         answer: 'Good'
     },
     {
-        question: 'Hi How Are You Today?',
+        question: 'Question Two?',
         index: 'Question 2 out of 10',
         choices: ['Nope','EH','Hanging On','Dying'],
         answer: 'Dying'
     },
     {
-        question: 'Hi How Are You Yesterday?',
+        question: 'Question Three?',
         index: 'Question 3 out of 10',
         choices: ['Nope','EH','Hanging On','Dying'],
         answer: 'Dying'
     },
     {
-        question: 'Hi How Are You Today?',
+        question: 'Question Four?',
         index: 'Question 4 out of 10',
         choices: ['Nope','EH','Hanging On','Dying'],
         answer: 'Dying'
     },
     {
-        question: 'Hi How Are You Today?',
-        index: 'Question 4 out of 10',
-        choices: ['Nope','EH','Hanging On','Dying'],
-        answer: 'Dying'
-    },
-    {
-        question: 'Hi How Are You Today?',
+        question: 'Question Five?',
         index: 'Question 5 out of 10',
         choices: ['Nope','EH','Hanging On','Dying'],
         answer: 'Dying'
     },
     {
-        question: 'Hi How Are You Today?',
+        question: 'Question Six?',
         index: 'Question 6 out of 10',
         choices: ['Nope','EH','Hanging On','Dying'],
         answer: 'Dying'
     },
     {
-        question: 'Hi How Are You Today?',
+        question: 'Question Seven?',
         index: 'Question 7 out of 10',
         choices: ['Nope','EH','Hanging On','Dying'],
         answer: 'Dying'
     },
     {
-        question: 'Hi How Are You Today?',
+        question: 'Question Eight?',
         index: 'Question 8 out of 10',
+        choices: ['Nope','EH','Hanging On','Dying'],
+        answer: 'Dying'
+    },
+    {
+        question: 'Question Nine?',
+        index: 'Question 9 out of 10',
+        choices: ['Nope','EH','Hanging On','Dying'],
+        answer: 'Dying'
+    },
+    {
+        question: 'Question Ten?',
+        index: 'Question 10 out of 10',
         choices: ['Nope','EH','Hanging On','Dying'],
         answer: 'Dying'
     }
@@ -86,18 +92,23 @@ function beginGame() {
         $(event.target).parent().hide();
 
             questions.forEach(element => {
+
                 var quizBox = $('<div class="contain question questionBox quizBox"><div class="questionTitle quizBoxTitle"><h1 class="spacer questionTitleText"></h1><div class=iconContainer id=itemContainer><div class="customIcon transition userPoints"title=Points>0</div><div class="customIcon transition iconTimer"title=Timer>60</div><div class="customIcon transition help"title=Help>?</div></div></div><div class=lineSep></div><h2 class="outOf questionIndex"></h2><p class="spacer questionP">Please choose one of the following answers!<ul class="answerChoices list-group"><li class="answer list-item">Variables<li class="answer list-item">Event Listeners<li class="answer list-item">DOM Maniplutaion<li class="answer list-item">Functions</ul></div>');
 
+                $(event.target).parent().parent().append(quizBox);
+
+
+               
                 for (var i = 0; i < questions.length; i++) {
                     var questionTitle = $('.questionTitleText');
                     var questionIndex = $('.questionIndex');
                     questionTitle.text(questions[i].question);
                     questionIndex.text(questions[i].index);
+                    console.log(questions[i].question);
+                    console.log(questions[i].index);
                 }
 
-                $(event.target).parent().parent().append(quizBox);
-
-            });
+            })
     })
 
 // Function Invokations
