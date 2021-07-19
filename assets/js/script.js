3882a7cbae5ae3b52b015a2fd03c61df906118e6
Rakib Ahmed <rakib987@gmail.com>
2021-07-19 06:38:18 -0400

    Buttons Logging

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,25 +16,25 @@ var gameButton = $('.gameButton');
 // Quiz Variables
 var questions = [
     {
-        question: 'Question One?',
-        index: 'Question 1 out of 10',
-        choices: ['Good','OK','Fantastic','Bad'],
-        answer: 'Good'
+        question: 'Which of these is NOT a Valid Javascript data type?',
+        index: 'Question One out of Ten',
+        choices: ['Null','Object','Symbol','Signal'],
+        answer: 'Signal'
     },
     {
-        question: 'Question Two?',
-        index: 'Question 2 out of 10',
-        choices: ['Nope','EH','Hanging On','Dying'],
+        question: 'Question Two',
+        index: 'Is JavaScript a case-sensitive language?',
+        choices: ['Yes','No'],
+        answer: 'Yes'
+    },
+    {
+        question: 'In Javascript, which of these is NOT a valid way to store data?',
+        index: '3 out of 10',
+        choices: ['Local Storage','Public Storage','Session Storage','Variables'],
         answer: 'Dying'
     },
     {
-        question: 'Question Three?',
-        index: 'Question 3 out of 10',
-        choices: ['Nope','EH','Hanging On','Dying'],
-        answer: 'Dying'
-    },
-    {
-        question: 'Question Four?',
+        question: 'Question Four',
         index: 'Question 4 out of 10',
         choices: ['Nope','EH','Hanging On','Dying'],
         answer: 'Dying'
@@ -89,33 +89,30 @@ function beginGame() {
         $(event.target).parent().hide();
 
             questions.forEach(element => {
-
-                // var questionBox = $('<div class="contain question questionBox quizBox"><div class="questionTitle quizBoxTitle"><h1 class="spacer questionTitleText"></h1><div class=iconContainer id=itemContainer><div class="customIcon transition userPoints"title=Points>0</div><div class="customIcon transition iconTimer"title=Timer>60</div><div class="customIcon transition help"title=Help>?</div></div></div><div class=lineSep></div><h2 class="outOf questionIndex"></h2><p class="spacer questionP">Please choose one of the following answers!<ul class="answerChoices list-group"><li class="answer list-item"></li><li class="answer list-item"></li><li class="answer list-item"></li><li class="answer list-item"></li></ul></div>');
-
-                var questionBox = $('<div class="contain question questionBox quizBox"><div class="questionTitle quizBoxTitle"><h1 class="spacer questionTitleText"></h1><div class=iconContainer id=itemContainer><div class="customIcon transition userPoints"title=Points>0</div><div class="customIcon transition iconTimer"title=Timer>60</div><div class="customIcon transition help"title=Help>?</div></div></div><div class=lineSep></div><h2 class="outOf questionIndex"></h2><p class="spacer questionP">Please choose one of the following answers!<ul class="answerChoices list-group"></ul></div>');
-
+                var questionBox = $('<div class="contain question questionBox quizBox"><div class="questionTitle quizBoxTitle"><h1 class="spacer questionTitleText"></h1><div class=iconContainer id=itemContainer><div class="customIcon transition userPoints"title=Points>0</div><div class="customIcon transition iconTimer"title=Timer>60</div><div class="customIcon transition help"title=Help>?</div></div></div><div class=lineSep></div><h2 class="outOf questionIndex"></h2><ul class="answerChoices list-group"></ul></div>');
                 $(event.target).parent().parent().append(questionBox);
-
             })
 
             for (var i = 0; i < questions.length; i++) {
+
                 var questionTitles = $('.questionTitleText');
                 var questionIndexes = $('.questionIndex');
                 questionTitles[i].textContent = questions[i].question;
                 questionIndexes[i].textContent = questions[i].index;
 
-                questions[i].choices.forEach(choice => {
-                    var answerChoices = $('.answerChoices');
-                    var answerButton = $('<li></li>');
-                    answerButton.innerText = questions[i].choices;
-                    answerButton.addClass('answer');
-                    answerButton.addClass('list-item');
-                    console.log(answerButton);
-                    answerChoices[i].append(answerButton);
-                })
+                    questions[i].choices.forEach(choice => {
+                        var answerChoices = $('.answerChoices');
+                        var answerButton = $('<li>');
+                        answerButton.addClass('answer');
+                        answerButton.addClass('list-item');
+                        for (var j = 0; j < questions[i].choices.length; j++) {
+                            answerButton.text(questions[i].choices[j]);
+                            answerChoices.append(answerButton[j]);
+                        }
+                    })
+
             }
-
-
+                
     })
 
 // Function Invokations
