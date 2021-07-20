14e9e46668a87785b85ca4c1935b10a7285fb42c
Rakib Ahmed <rakib987@gmail.com>
2021-07-19 20:53:19 -0400

    Buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,21 +17,21 @@ var gameButton = $('.gameButton');
 var questions = [
     {
         question: 'Which of these is NOT a Valid Javascript data type?',
-        index: 'Question One out of Ten',
+        index: 'Question 1 out of 10',
         choices: ['Null','Object','Symbol','Signal'],
         answer: 'Signal'
     },
     {
-        question: 'Question Two',
-        index: 'Is JavaScript a case-sensitive language?',
+        question: 'Is JavaScript a case-sensitive language?',
+        index: 'Question 2 out of 10',
         choices: ['Yes','No'],
         answer: 'Yes'
     },
     {
-        question: 'In Javascript, which of these is NOT a valid way to store data?',
-        index: '3 out of 10',
+        question: 'Which of these is NOT a valid way to store user data?',
+        index: 'Question 3 out of 10',
         choices: ['Local Storage','Public Storage','Session Storage','Variables'],
-        answer: 'Dying'
+        answer: 'Public Storage'
     },
     {
         question: 'Question Four',
@@ -89,7 +89,7 @@ function beginGame() {
         $(event.target).parent().hide();
 
             questions.forEach(element => {
-                var questionBox = $('<div class="contain question questionBox quizBox"><div class="questionTitle quizBoxTitle"><h1 class="spacer questionTitleText"></h1><div class=iconContainer id=itemContainer><div class="customIcon transition userPoints"title=Points>0</div><div class="customIcon transition iconTimer"title=Timer>60</div><div class="customIcon transition help"title=Help>?</div></div></div><div class=lineSep></div><h2 class="outOf questionIndex"></h2><ul class="answerChoices list-group"></ul></div>');
+                var questionBox = $('<div class="contain question questionBox quizBox"><div class="questionTitle quizBoxTitle"><h1 class="spacer questionTitleText"></h1><div class=iconContainer id=itemContainer><div class="customIcon transition userPoints"title=Points>0</div><div class="customIcon transition iconTimer"title=Timer>60</div><div class="customIcon transition help"title=Help>?</div></div></div><div class=lineSep></div><ul class="answerChoices list-group"></ul><h2 class="outOf questionIndex"></h2></div>');
                 $(event.target).parent().parent().append(questionBox);
             })
 
@@ -100,15 +100,16 @@ function beginGame() {
                 questionTitles[i].textContent = questions[i].question;
                 questionIndexes[i].textContent = questions[i].index;
 
-                    questions[i].choices.forEach(choice => {
+                    questions[i].choices.forEach(function(choices) {
                         var answerChoices = $('.answerChoices');
                         var answerButton = $('<li>');
                         answerButton.addClass('answer');
                         answerButton.addClass('list-item');
                         for (var j = 0; j < questions[i].choices.length; j++) {
                             answerButton.text(questions[i].choices[j]);
-                            answerChoices.append(answerButton[j]);
+                            console.log(questions[i].choices[j]);
                         }
+                        answerChoices.append(answerButton[i]);
                     })
 
             }
