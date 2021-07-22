d345bd83ecec0e16d159d88f00f80275d2e89ace
Rakib Ahmed <rakib987@gmail.com>
2021-07-22 08:19:27 -0400

    Dynamic Elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,54 @@
+// Dynamic Controller
+
 // Welcome to the Coding Quiz!
 console.log('Coding Quiz!');
 
-// Declaring Variables
-var showScoresLink = $('#showScoresLink');
+// Game Variables
+var userPoints = 0; // User Starting Points
+var maxHighScores = 5; // Max High Scores to Store
+// Creating Dynamic Time Controller
+var timerRange = $('<input>');
+// Dynamic Timers
 var topTimer = $('.timeLeft');
-var countDownTimer = 60;
-var main = $('main');
-var totalQuestions = $('.totalQuestions');
-var gameButton = $('.gameButton');
-var userPoints = 0;
-var maxHighScores = 5;
-var showScoresElement = $('#scoresHover');
-
-// High Scores
-var highScores = JSON.parse(localStorage.getItem('High Scores')) || [];
-// Hide High Scores if array is empty
-if (highScores.length === 0) showScoresElement.html('No Scores To Show Yet!');
-if (highScores.length > 0) {
-    setTimeout(function() {
-        showScoresElement.addClass('fadeInOut');
-    }, 300)
-}
-highScores.map(score => {
-    var highScoreItems = $('<div class="statistic">');
-    var blueSlashes = $('<span style="color: var(--neutral);"> // </span>');
-    var blueSlashes2 = $('<span style="color: var(--neutral);"> // </span>');
-    var scoreName = score.name;
-    var scoreNameElement = $('<div class="scoreName">');
-    scoreNameElement.append(scoreName);
-    var scoreScore = score.score;
-    var scoreScoreElement = $('<div class="scoreScore">');
-    scoreScoreElement.append(scoreScore + ' points');
-    var scoreTimeLeft = score.timeLeft;
-    var scoreTimeLeftElement = $('<div class="scoreTimeLeft">');
-    scoreTimeLeftElement.append(scoreTimeLeft + ' left');
-
-    highScoreItems.append(scoreNameElement);
-    highScoreItems.append(blueSlashes);
-    highScoreItems.append(scoreScoreElement);
-    highScoreItems.append(blueSlashes2);
-    highScoreItems.append(scoreTimeLeftElement);
-   showScoresElement.append(highScoreItems);
-   showScoresLink.on('click', function(event) {
-       var quizBox = $('.quizBox');
-    //    quizBox.after(highScoreItems);
-       showScoresElement.append(highScoreItems);
-   })
+topTimer.css({'display':'flex','justify-content':'space-between','align-content':'center','width':'14em','align-items':'center','transition':'300 ms ease-in-out'});
+timerRange.attr('type','range');
+timerRange.attr('min','15');
+timerRange.attr('max','99');
+timerRange.attr('value','60');
+timerRange.attr('class','timerRange');
+timerRange.attr('id','timerRange');
+var countDownTimer = timerRange.val(); // This is the number that the timer will begin counting down from
+var timeRemaining = $('.timeRemaining');
+timeRemaining.html(countDownTimer);
+var dynamicTimerIcon = $('<i>');
+dynamicTimerIcon.attr('class','fas');
+dynamicTimerIcon.addClass('fa-stopwatch');
+topTimer.html(dynamicTimerIcon);
+topTimer.append(' | ');
+var dynamicTime = $('<div id="dynamicTime">');
+var dynamicTimeNumber = $('<span id="dynamicTimeNumber">');
+topTimer.append(dynamicTime);
+dynamicTimeNumber.html(countDownTimer);
+timerRange.on('input',function(event) {
+    var timerRangeValue = $(event.target).val();
+    countDownTimer = timerRangeValue;
+    var timeRemaining = $('.timeRemaining');
+    timeRemaining.html(countDownTimer);
+    dynamicTimeNumber.html(countDownTimer);
 })
+topTimer.append(timerRange);
+dynamicTime.append(dynamicTimeNumber);
+dynamicTime.append(timerRange);
+
+// Declaring Variables
+var main = $('main');
+// Score Link and Hover Box
+var showScoresLink = $('#showScoresLink');
+var showScoresElement = $('#scoresHover');
+// Main Quiz Box
+var quizBox = $('.quizBox');
+// Begin Game Button
+var gameButton = $('.gameButton');
 
 // Quiz Variables
 var questions = [
@@ -122,8 +124,91 @@ var questions = [
     }
 ];
 
-// Functions
+// Dynamic Question Total
+var totalQuestions = $('.totalQuestions');
+totalQuestions.html(questions.length);
+
+// Sounds
+var correctSound = new Audio();
+correctSound.src = './assets/sounds/Correct.mp3';
+var wrongSound = new Audio();
+wrongSound.src = './assets/sounds/Error.mp3';
+var victorySound = new Audio();
+victorySound.src = './assets/sounds/Wow.mp3';
+var timeoutSound = new Audio();
+timeoutSound.src = './assets/sounds/wahwahwah.mp3';
+
+// High Scores
+var highScores = JSON.parse(localStorage.getItem('High Scores')) || [];
+
+// High Scores Display
+highScores.map(score => {
+
+    // For Each Score Stored, Create & Execute
+    var highScoreItems = $('<div class="statistic">');
+    var blueSlashes = $('<span style="color: var(--neutral);"> // </span>');
+    var blueSlashes2 = $('<span style="color: var(--neutral);"> // </span>');
+    var scoreName = score.name;
+    var scoreNameElement = $('<div class="scoreName">');
+    scoreNameElement.append(scoreName);
+    var scoreScore = score.score;
+    var scoreScoreElement = $('<div class="scoreScore">');
+    scoreScoreElement.append(scoreScore + ' points');
+    var scoreTimeLeft = score.timeLeft;
+    var scoreTimeLeftElement = $('<div class="scoreTimeLeft">');
+    scoreTimeLeftElement.append(scoreTimeLeft + ' left');
+
+    highScoreItems.append(scoreNameElement);
+    // Created Blue slashes for Styling
+    highScoreItems.append(blueSlashes);
+    highScoreItems.append(scoreScoreElement);
+    // Had to make two for some reason?
+    highScoreItems.append(blueSlashes2);
+    highScoreItems.append(scoreTimeLeftElement);
+   showScoresElement.append(highScoreItems);
+
+   showScoresLink.on('click', function(event) {
+       showScoresElement.append(highScoreItems);
+   })
+
+})
+
+// If High Scores Available Fade-In-Out Animation
+if (highScores.length > 0) {
+
+    setTimeout(function() {
+        showScoresElement.addClass('fadeInOut');
+    }, 1500);
+
+    // Creating Button to Clear Local Storage // Clear High Scores
+    var clearButton = $('<button>');
+    clearButton.html('Clear Scores');
+    clearButton.on('click', function(event) {
+        localStorage.removeItem('High Scores');
+        showScoresElement.children().fadeOut(1000);
+        setTimeout(function() {
+            showScoresElement.children().remove();
+        }, 1100);
+        var noScoresMessage = $('<div>');
+        noScoresMessage.html('No Scores to Show Yet!');
+        setTimeout(function() {
+            noScoresMessage.fadeIn(1000);
+        }, 300);
+        showScoresElement.append(noScoresMessage);
+        return false;
+    })
+    // Appending Button
+showScoresElement.append(clearButton);
+}
+
+// Begin Game
 function beginGame() {
+
+    // Timer Styling
+    timerRange.remove();
+    topTimer.css({'display':'flex','justify-content':'space-between','align-content':'center','width':'3.9em','align-items':'center','transition':'300 ms ease-in-out'});
+    topTimer.fadeOut(10);
+    topTimer.fadeIn(3300);
 
     // Count Down Timer Start
     var countDown = setInterval(function() {
@@ -135,10 +220,12 @@ function beginGame() {
             clearInterval(countDown);
             var questionBox = $('.questionBox');
             questionBox.addClass('timeOut');
-            // Time Ran Out Screen
 
             //             //             //      GAME END FUNCTIONS       //             //             //             //
-            questionBox.html('<div class="questionTitle quizBoxTitle"><h1 class="spacer questionTitleText"><b>Time Is Up!</b></h1><div class="totalPointsElement"><div class="iconContainer outerIconContainer"><div class="iconContainer" id="itemContainer">Time Remaining: <div class="customIcon transition timeRemaining" title="Time">60</div></div><div class="iconContainer" id="itemContainer">Total Points: <div class="customIcon transition userPoints"title="Points">0</div></div></div></div></div><div class=lineSepEnd></div><div id="formEntry" class="stats"><div id="userStats"><div class="stats"><div id="userName">USER </div><div id="userPointsElement">Points: </div><div id="userTimeRemaining">Time Left: </div><div id="date">Today: </div></div><form id="entryForm"><input id="name" type="name" name="name" placeholder="Enter your Name"><button type="submit" id="submitButton">Submit Score</button></form></div></div>');
+
+            // Time Ran Out Screen
+            timeoutSound.play();
+            questionBox.html('<div class="questionTitle quizBoxTitle"><h1 class="spacer questionTitleText"><b>Time Is Up!</b></h1><div class="totalPointsElement"><div class="iconContainer outerIconContainer"><div class="iconContainer" id="itemContainer">Time Remaining: <div class="customIcon transition timeRemaining" title="Time">60</div></div><div class="iconContainer" id="itemContainer">Total Points: <div class="customIcon transition userPoints"title="Points">0</div></div></div></div></div><div class=lineSepEnd></div><div id="formEntry" class="stats"><div id="userStats"><div class="stats"><div id="userName">User </div><div id="userPointsElement">Points: </div><div id="userTimeRemaining">Time Left: </div><div id="date">Today: </div></div><form id="entryForm"><input id="name" type="name" name="name" placeholder="Enter your Name"><button type="submit" id="submitButton">Submit Score</button></form></div></div>');
             $('.userPoints').html(userPoints * 10);
             var printName = main.children().children().find('#userName');
             var printPoints = main.children().children().find('#userPointsElement');
@@ -159,6 +246,19 @@ function beginGame() {
                 $(event.target).parent().parent().parent().addClass('endScreen');
                 $(event.target).parent().parent().parent().siblings().remove();
                 var userName = $(event.target).children().val();
+                // Input Validation
+                if (!$(event.target).children().val()) {
+                    var alertMessage = $('<div>You Must Enter a Name to Submit Score!</div>');
+                    alertMessage.attr('id','alertMessage');
+                    alertMessage.attr('class','reinitializeMessage');
+                    alertMessage.fadeIn();
+                    endScreen.append(alertMessage);
+                    setTimeout(function() {
+                        alertMessage.fadeOut();
+                    }, 3000)
+                    alertMessage.remove();
+                    return;
+                }
                 printName.html(userName);
                 printName.attr('style','text-transform:uppercase');
                 var appendName = $('<div>');
@@ -181,7 +281,7 @@ function beginGame() {
                 // Reload Game After Score is Stored
                 setTimeout(function reloadGame() {
                     location.reload(true);
-                }, 1500);
+                }, 1000);
                 })
         }
 
@@ -202,6 +302,7 @@ function beginGame() {
                 // Count Down Timer Stop
                 console.log('Game Over!');
                 $('.userPoints').html(userPoints * 10);
+                victorySound.play();
                 // Updated Statistics
                 var currentTimeRemaining = localStorage.getItem('Current Time Remaining');
                 var currentUserPoints = localStorage.getItem('Current Points');
@@ -216,22 +317,28 @@ function beginGame() {
     }, 1000);
 }
 
+// On Begin Game Button
     main.on('click', '.gameButton', function(event) {
 
+        // Call back to Main Begin Game Function
         beginGame();
+
+        // Hides the 'Begin Quiz' Element
         $(event.target).parent().hide();
 
-        // Creating a div for each question in our array
+        // Creating a Div for each question in our array
+        // These Divs will store our questions
         questions.forEach(element => {
-            var questionBox = $('<div class="contain question questionBox quizBox"><div class="questionTitle quizBoxTitle"><h1 class="spacer questionTitleText"></h1><div class="iconContainer outerIconContainerQuestions"><div class=iconContainer id=itemContainer>Time Remaining: <div class="customIcon transition timeRemaining" title="Time">60</div></div><div class="iconContainer" id="itemContainer">Total Points: <div class="customIcon transition userPoints"title=Points>0</div></div></div></div><div class=lineSep></div><ul class="answerChoices list-group"></ul><h2 class="outOf questionIndex"></h2></div>');
+            var questionBox = $('<div class="contain question questionBox quizBox"><div class="questionTitle quizBoxTitle"><h1 class="spacer questionTitleText"></h1><div class="iconContainer outerIconContainerQuestions"><div class=iconContainer id=itemContainer>Time Remaining: <div class="customIcon transition timeRemaining" title="Time">' + countDownTimer + '</div></div><div class="iconContainer" id="itemContainer">Total Points: <div class="customIcon transition userPoints"title=Points>0</div></div></div></div><div class=lineSep></div><ul class="answerChoices list-group"></ul><h2 class="outOf questionIndex"></h2></div>');
             $(event.target).parent().parent().append(questionBox);
             var questionBox = $('.questionBox');
             questionBox.attr('id', 'questionBox');
         })
 
-        //             //             //      GAME END FUNCTIONS       //             //             //           //
+        //             //             //      GAME END FUNCTIONS 2       //             //             //           //
+
         // Quiz Finished Screen
-        var endScreen = $('<div class="endScreen contain question questionBox quizBox hide timeOut"><div class="questionTitle quizBoxTitle"><h1 class="spacer questionTitleText"><b>Quiz Completed!</b></h1><div class="totalPointsElement"><div class="outerIconContainer iconContainer"><div class=iconContainer id=itemContainer>Time Remaining: <div class="customIcon transition timeRemaining" title="Time">60</div></div><div class=iconContainer id=itemContainer>Total Points: <div class="customIcon transition userPoints"title="Points">0</div></div></div></div></div><div class=lineSepEnd></div><div id="formEntry" class="stats"><div id="userStats"><div class="stats"><div id="userName">USER </div><div id="userPointsElement">Points: </div><div id="userTimeRemaining">Time Left: </div><div id="date">Today: </div></div><form id="entryForm"><input id="name" type="name" name="name" placeholder="Enter your Name"><button type="submit" id="submitButton">Submit Score</button></form></div></div></div>');
+        var endScreen = $('<div class="endScreen contain question questionBox quizBox hide timeOut"><div class="questionTitle quizBoxTitle"><h1 class="spacer questionTitleText"><b>Quiz Completed!</b></h1><div class="totalPointsElement"><div class="outerIconContainer iconContainer"><div class=iconContainer id=itemContainer>Time Remaining: <div class="customIcon transition timeRemaining" title="Time"></div></div><div class=iconContainer id=itemContainer>Total Points: <div class="customIcon transition userPoints"title="Points">0</div></div></div></div></div><div class=lineSepEnd></div><div id="formEntry" class="stats"><div id="userStats"><div class="stats"><div id="userName">User </div><div id="userPointsElement">Points: </div><div id="userTimeRemaining">Time Left: </div><div id="date">Today: </div></div><form id="entryForm"><input id="name" type="name" name="name" placeholder="Enter your Name"><button type="submit" id="submitButton">Submit Score</button></form></div></div></div>');
         main.append(endScreen);
         var printName = main.children().children().find('#userName');
         var printDate = main.children().children().find('#date');
@@ -248,8 +355,20 @@ function beginGame() {
             $(event.target).parent().parent().parent().addClass('endScreen');
             $(event.target).parent().parent().parent().siblings().remove();
             var userName = $(event.target).children().val();
+            // Input Validation
+            if (!$(event.target).children().val()) {
+                var alertMessage = $('<div>You Must Enter a Name to Submit Score!</div>');
+                alertMessage.attr('id','alertMessage');
+                alertMessage.attr('class','reinitializeMessage');
+                alertMessage.fadeIn();
+                endScreen.append(alertMessage);
+                setTimeout(function() {
+                    alertMessage.fadeOut();
+                }, 3000)
+                alertMessage.remove();
+                return;
+            }
             printName.html(userName);
-            printName.attr('style','text-transform:uppercase');
             var appendName = $('<div>');
             appendName.attr('id','goodJobMessage');
             appendName.text('Good Job, ' + userName);
@@ -270,7 +389,7 @@ function beginGame() {
             // Reload Game After Score is Stored
             setTimeout(function reloadGame() {
                 location.reload(true);
-            }, 1500);
+            }, 1000);
         })
 
         for (var i = 0; i < questions.length; i++) {
@@ -304,34 +423,41 @@ function beginGame() {
                         $(event.target).parent().parent().next().removeClass('hide');
                         $(event.target).parent().parent().hide();
                         clearInterval(nextQuestion);
-                    }, 350);
+                    }, 750);
 
                     if ($(event.target).data('value') === correctAnswer) {
 
                         // Change Button color to Green if Correct
                         $(event.target).addClass('correct');
 
-                        // Add 1 Point
+                        // Play Correct Sound
+                        correctSound.play();
+
+                        // Add 1 Point // Well, more like 0.25 points? Read more below
                         var userPointsElement = $('.userPoints');
                         if (userPoints >= 0 && userPoints < questions.length) {
-                            // This is nested in a forEach loop
-                            // So everytime i did userPoints++, it would add 1 four times, once for each answer choice
+                            // This is nested in a forEach loop ^
+                            // So everytime i did userPoints++
+                            // It would add +1 four times
+                            // Once for each answer choice
                             // Instead I did '+ 1/4'
-                            userPoints = userPoints + 1/4;
+                            userPoints = userPoints + 1 / 4;
                             userPointsElement.text(userPoints);
-                        } // else {
-                        //     userPointsElement.text(0);
-                        // }
+                        }
                         userPointsElement.text(userPoints);
                     }
                     // Change Button Color to Correct
                     else if($(event.target).data('value') === incorrectAnswers || $(event.target).data('value') === incorrectAnswers2 || $(event.target).data('value') === incorrectAnswers3) {
 
-                         // Change Button color to Green if Correct
+                        // Change Button color to Red if Wrong
                         $(event.target).addClass('wrong');
+
+                        // Play Wrong Sound
+                        wrongSound.play();
                     }
                 })
             })
+
             // Hide other boxes
             if (i < questions.length-1) {
                 var newi = i + 1;
@@ -339,3 +465,8 @@ function beginGame() {
             } 
         }
     })
+
+// Hide High Scores if array is empty
+if (highScores.length === 0) {
+    showScoresElement.html('No Scores To Show Yet!');
+}
